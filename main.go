803b64c1f46ff7a5ec7a8fadd7aa381b021da059
Alexey Sibyakin <sibyakin@yahoo.com>
2017-10-21 16:44:34 +0900

    apiv1 group added

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	mux := gin.Default
-	mux.GET("/api/v1/images", imageslist)
-	mux.Run()
+	mux := gin.Default()
+	mux.LoadHTMLGlob("templates/*.html")
+	mux.Static("/static", "./static")
+
+	apiv1 := mux.Group("/api/v1")
+	apiv1.GET("/images", imageslist)
+	apiv1.GET("/images/:id", imagesdetail)
+	apiv1.PUT("/images", imagesadd)
+
+	log.Fatalln(mux.Run())
 }
 
 func imageslist(ctx *gin.Context) {
-	ctx.JSON(200, "Hello world!")
+	ctx.JSON(200, gin.H{})
+}
+
+func imagesdetail(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{})
+}
+
+func imagesadd(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{})
 }
