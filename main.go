c2111eaf705a37e5397dfdbbfa576169a6bcd013
Alexey Sibyakin <sibyakin@yahoo.com>
2017-10-22 17:27:59 +0900

    general cleanups

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const workdir = "./public/"
+
 // String64 is a Gin request binding for json/base64 parsing
 type String64 struct {
 	Name    string `json:"name" binding:"required"`
@@ -37,39 +39,36 @@ func main() {
 	// In future we can easily add new versions
 	// of our API and It will be painless (hopefully)
 	apiv1 := mux.Group("/api/v1")
-	apiv1.GET("/images", imgListV1)
-	apiv1.POST("/images", imgAddV1)
-	apiv1.GET("/images/url", imgAddURLV1)
-	apiv1.POST("/images/json", imgAddJSONV1)
+	apiv1.POST("/images", imgAdd)
+	apiv1.GET("/images/url", imgAddURL)
+	apiv1.POST("/images/json", imgAddJSON)
 
 	log.Fatalln(mux.Run())
 }
 
 func createThumb(filename string) {
-	img, _ := imaging.Open("./public/" + filename)
+	img, _ := imaging.Open(workdir + filename)
 	thumb := imaging.Thumbnail(img, 100, 100, imaging.Box)
 	result := imaging.New(100, 100, color.NRGBA{0, 0, 0, 0})
 	result = imaging.Paste(result, thumb, image.Pt(0, 0))
-	imaging.Save(result, "./public/thumb_"+filename)
+	imaging.Save(result, workdir+"thumb_"+filename)
 }
 
-func imgListV1(ctx *gin.Context) {
-	ctx.HTML(200, "list.html", gin.H{})
-}
-
-func imgAddV1(ctx *gin.Context) {
+func imgAdd(ctx *gin.Context) {
 	form, _ := ctx.MultipartForm()
 	files := form.File["files[]"]
 	for _, file := range files {
-		err := ctx.SaveUploadedFile(file, "./public/"+file.Filename)
+		err := ctx.SaveUploadedFile(file, workdir+file.Filename)
 		if err != nil {
-			ctx.String(500, "Internal Server Error")
+			ctx.String(500, "Unable to save one or more files!")
+			return
 		}
+		createThumb(file.Filename)
 	}
 	ctx.String(200, fmt.Sprintf("%d file(s) uploaded successfully!\n", len(files)))
 }
 
-func imgAddJSONV1(ctx *gin.Context) {
+func imgAddJSON(ctx *gin.Context) {
 	var json String64
 	err := ctx.BindJSON(&json)
 	if err != nil {
@@ -77,7 +76,7 @@ func imgAddJSONV1(ctx *gin.Context) {
 		return
 	}
 	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(json.Image64))
-	file, err := os.Create("./public/" + json.Name)
+	file, err := os.Create(workdir + json.Name)
 	if err != nil {
 		ctx.String(500, "Unable to save file on server!\n")
 		return
@@ -92,7 +91,7 @@ func imgAddJSONV1(ctx *gin.Context) {
 	ctx.String(200, "Image saved successfully\n")
 }
 
-func imgAddURLV1(ctx *gin.Context) {
+func imgAddURL(ctx *gin.Context) {
 	var form URLForm
 	err := ctx.Bind(&form)
 	if err != nil {
@@ -101,7 +100,7 @@ func imgAddURLV1(ctx *gin.Context) {
 	}
 	tokens := strings.Split(form.URLstring, "/")
 	filename := tokens[len(tokens)-1]
-	file, err := os.Create("./public/" + filename)
+	file, err := os.Create(workdir + filename)
 	if err != nil {
 		ctx.String(500, "Unable to download requested file!\n")
 		return
@@ -118,5 +117,6 @@ func imgAddURLV1(ctx *gin.Context) {
 		ctx.String(500, "Unable to download requested file!\n")
 		return
 	}
+	createThumb(filename)
 	ctx.String(200, "Image saved successfully\n")
 }
