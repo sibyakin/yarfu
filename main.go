3f36d3e567ca7954eee9ebefdd27b63e2a714233
Alexey Sibyakin <sibyakin@yahoo.com>
2017-10-22 15:37:26 +0900

    downloading via url implemented

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
@@ -17,6 +18,11 @@ type String64 struct {
 	Image64 string `json:"image64" binding:"required"`
 }
 
+// URLForm here is a Gin request binding for url parsing
+type URLForm struct {
+	URLstring string `form:"url" binding:"required"`
+}
+
 func main() {
 	mux := gin.Default()
 	mux.LoadHTMLGlob("templates/*.html")
@@ -26,10 +32,10 @@ func main() {
 	// of our API and It will be painless (hopefully)
 	apiv1 := mux.Group("/api/v1")
 	apiv1.GET("/images", imgListV1)
-	apiv1.GET("/images/:id", imgDetailV1)
+	apiv1.GET("/images/url", imgAddURLV1)
+	//apiv1.GET("/images/:id", imgDetailV1)
 	apiv1.POST("/images", imgAddV1)
 	apiv1.POST("/images/json", imgAddJSONV1)
-	apiv1.POST("/images/url", imgAddURLV1)
 
 	log.Fatalln(mux.Run())
 }
@@ -38,9 +44,9 @@ func imgListV1(ctx *gin.Context) {
 	ctx.HTML(200, "list.html", gin.H{})
 }
 
-func imgDetailV1(ctx *gin.Context) {
-	ctx.HTML(200, "detail.html", gin.H{})
-}
+//func imgDetailV1(ctx *gin.Context) {
+//	ctx.HTML(200, "detail.html", gin.H{})
+//}
 
 func imgAddV1(ctx *gin.Context) {
 	form, _ := ctx.MultipartForm()
@@ -58,7 +64,6 @@ func imgAddJSONV1(ctx *gin.Context) {
 	var json String64
 	err := ctx.BindJSON(&json)
 	if err != nil {
-		fmt.Println(err)
 		ctx.String(500, "Unable to parse request body!\n")
 		return
 	}
@@ -74,9 +79,34 @@ func imgAddJSONV1(ctx *gin.Context) {
 		ctx.String(500, "Unable to save file on server!\n")
 		return
 	}
-	ctx.String(200, "Image saved successfully\n")
+	ctx.JSON(200, "Image saved successfully\n")
 }
 
 func imgAddURLV1(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{})
+	var form URLForm
+	err := ctx.Bind(&form)
+	if err != nil {
+		ctx.String(500, "Unable to parse request body!\n")
+		return
+	}
+	tokens := strings.Split(form.URLstring, "/")
+	filename := tokens[len(tokens)-1]
+	file, err := os.Create("./public/" + filename)
+	if err != nil {
+		ctx.String(500, "Unable to download requested file!\n")
+		return
+	}
+	defer file.Close()
+	response, err := http.Get(form.URLstring)
+	if err != nil {
+		ctx.String(500, "Unable to download requested file!\n")
+		return
+	}
+	defer response.Body.Close()
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		ctx.String(500, "Unable to download requested file!\n")
+		return
+	}
+	ctx.String(200, "Image saved successfully\n")
 }
